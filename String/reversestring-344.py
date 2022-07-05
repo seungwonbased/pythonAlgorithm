a8e04a0e597be4ccdeb561ed6a61e753337b1cb5
baeseungwon <21storeo@gmail.com>
2022-07-05 23:56:44 +0900

    msg

--- String/reversestring-344.py
+++ String/reversestring-344.py
@@ -1,10 +1,13 @@
 from typing import List
 
 
-def reverseStringByToPointer(s:List[str]) -> None:
+def reverseStringByToPointer(s: List[str]) -> None:
     left, right = 0, len(s) - 1
     while left < right:
         s[left], s[right] = s[right], s[left]
         left += 1
         right -= 1
 
+
+def reverseStringByPythonicWay(s: List[str]) -> None:
+    s.reverse()
